2d6c04a6f663dc62292f102aba9ad555d6ed39ba
Jordan Vidrine <jordan@jordanvidrine.com>
2022-02-09 15:14:10 -0600

    more

---
+++ javascripts/discourse/helpers/childIsActive.js
@@ -0,0 +1,15 @@
+import { registerUnbound } from "discourse-common/lib/helpers";
+
+registerUnbound("childIsActive", (currentRoute, parentSlug) => {
+  if (
+    !currentRoute ||
+    !currentRoute.attributes ||
+    !currentRoute.attributes.category
+  )
+    return false;
+
+  let currentPath =
+    currentRoute.attributes.modelParams.category_slug_path_with_id;
+
+  return currentPath.includes(parentSlug);
+});
