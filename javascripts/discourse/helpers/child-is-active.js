a6eefd9c226429d54a6da982e5cf003700fac438
David Taylor <david@taylorhq.com>
2024-03-27 17:27:42 +0000

    DEV: Update linting

--- javascripts/discourse/helpers/child-is-active.js
+++ javascripts/discourse/helpers/child-is-active.js
@@ -1,5 +1,5 @@
-import { registerUnbound } from "discourse-common/lib/helpers";
 import Category from "discourse/models/category";
+import { registerUnbound } from "discourse-common/lib/helpers";
 
 registerUnbound("child-is-active", (categoryId, parentCategoryId) => {
   const c = Category.findById(categoryId);
