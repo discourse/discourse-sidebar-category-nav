a01d291ca3afdfc1f651bf3087b5c6baee79d766
Jarek Radosz <jradosz@gmail.com>
2024-10-31 00:37:22 +0100

    DEV: Replace deprecated `registerUnbound` (#36)

--- javascripts/discourse/helpers/child-is-active.js
+++ javascripts/discourse/helpers/child-is-active.js
@@ -1,19 +1,13 @@
 import Category from "discourse/models/category";
-import { registerUnbound } from "discourse-common/lib/helpers";
 
-registerUnbound("child-is-active", (categoryId, parentCategoryId) => {
-  const c = Category.findById(categoryId);
-  if (!c) {
+export default function (categoryId, parentCategoryId) {
+  const category = Category.findById(categoryId);
+  if (!category) {
     return false;
   }
 
-  if (c.parentCategory?.id === parentCategoryId) {
-    return true;
-  }
-
-  if (c.parentCategory?.parentCategory?.id === parentCategoryId) {
-    return true;
-  }
-
-  return false;
-});
+  return (
+    category.parentCategory?.id === parentCategoryId ||
+    category.parentCategory?.parentCategory?.id === parentCategoryId
+  );
+}
