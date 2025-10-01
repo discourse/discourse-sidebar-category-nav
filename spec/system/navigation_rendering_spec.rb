5c1e5391c1b5dda8f826a335a614ca179b85a664
Kris <kris.aubuchon@discourse.org>
2025-10-01 09:32:36 -0400

    DEV: add tests to ensure sidebar renders on correct routes (#56)

---
+++ spec/system/navigation_rendering_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+describe "Navigation Rendering", type: :system do
+  let!(:category) { Fabricate(:category, name: "Test Category") }
+  let!(:topic) { Fabricate(:topic, category:) }
+  let!(:post) { Fabricate(:post, topic:) }
+
+  before do
+    upload_theme_component
+  end
+
+  context "when on discovery routes" do
+    it "renders navigation only once" do
+      visit("/")
+
+      expect(page).to have_selector(".category-sidebar-outlet", count: 1)
+    end
+
+    it "renders navigation on category pages" do
+      visit("/c/#{category.id}")
+
+      expect(page).to have_selector(".category-sidebar-outlet", count: 1)
+    end
+  end
+
+  context "when on topic routes" do
+    it "does render navigation" do
+      visit("/t/#{topic.id}")
+
+      expect(page).to have_selector(".category-sidebar-outlet")
+    end
+  end
+
+  context "when on categories page" do
+    it "renders navigation only once" do
+      visit("/categories")
+
+      expect(page).to have_selector(".category-sidebar-outlet", count: 1)
+    end
+  end
+
+  context "while elsewhere navigation does not render" do
+    it "does not render on user profiles" do
+      user = Fabricate(:user)
+      visit("/u/#{user.username}")
+
+      expect(page).to have_no_selector(".category-sidebar-outlet")
+    end
+  end
+end
--- spec/system/sub_category_toggle_spec.rb
+++ spec/system/sub_category_toggle_spec.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
 describe "Sub Category Toggle", type: :system do
-  let(:theme) do
-    parent_theme = Fabricate(:theme, name: "Parent Theme")
-    component = Fabricate(:theme, name: "Category Sidebar Navigation", component: true)
-    parent_theme.set_default!
-  end
+  let!(:category) { Fabricate(:category, name: "Test Category") }
+  let!(:subcategory) { Fabricate(:category, name: "Test Subcategory", parent_category: category) }
 
   before do
-    theme
     upload_theme_component
-    @category = Fabricate(:category, name: "Test Category")
-    @subcategory = Fabricate(:category, name: "Test Subcategory", parent_category: @category)
   end
 
   it "displays subcategories when toggle is clicked" do
@@ -22,16 +16,16 @@ describe "Sub Category Toggle", type: :system do
     expect(page).to have_selector(".category-sidebar-list-item__parent.show-children")
     expect(page).to have_selector(
       ".category-sidebar-list-item-link.subcategory-item",
-      text: @subcategory.name,
+      text: subcategory.name,
     )
   end
 
   it "displays subcategories when on category page" do
-    visit("/c/#{@category.id}")
+    visit("/c/#{category.id}")
     expect(page).to have_selector(".category-sidebar-list-item__parent.show-children")
     expect(page).to have_selector(
       ".category-sidebar-list-item-link.subcategory-item",
-      text: @subcategory.name,
+      text: subcategory.name,
     )
   end
 end
